6897fd54cb601f7373ea50dd7be11d81fbb0049b
Dennis Ping <idennisping@gmail.com>
2022-12-06 19:20:15 -0500

    Initial commit of new branch

--- controllers/users/users-controller.js
+++ controllers/users/users-controller.js
@@ -1,32 +1,34 @@
 import * as usersDao from "./users-dao.js";
 
+// Some trivial comment to put myself on the github repo
+
 const createUser = async (req, res) => {
   const newUser = req.body;
   const insertedUser = await usersDao.createUser(newUser);
   res.json(insertedUser);
-}
+};
 
-const findUsers = async(req, res) => {
+const findUsers = async (req, res) => {
   const users = await usersDao.findUsers();
   res.json(users);
-}
+};
 
-const updateUser = async(req, res) => {
+const updateUser = async (req, res) => {
   const userIdToUpdate = req.params.userId;
   const updates = req.body;
   const status = await usersDao.updateUser((userIdToUpdate, updates));
   res.json(status);
-}
+};
 
-const deleteUser = async(req, res) => {
+const deleteUser = async (req, res) => {
   const userIdToDelete = req.params.userId;
   const status = await usersDao.deleteUser(userIdToDelete);
   res.json(status);
-}
+};
 
 export default (app) => {
   app.post("/api/users", createUser);
   app.get("/api/users", findUsers);
   app.put("/api/users/:userId", updateUser);
   app.delete("/api/users/:userId", deleteUser);
-}
+};
